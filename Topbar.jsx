572c3ad894cbfc6b47b90d9d46c69a3856bf3d3d
Anil1013 <ruhil.13r@gmail.com>
2025-10-05 04:50:19 +0530

    Add files via upload

---
+++ Topbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+export default function Topbar(){
+  return (
+    <header className="topbar">
+      <div className="brand">
+        <img src="/logo.png" alt="Mob13r" />
+        <span className="title">Mob13r Dashboard</span>
+      </div>
+    </header>
+  )
+}
